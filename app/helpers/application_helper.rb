eb5388d5e2b21cbaf3387d057168b8abf51485ad
Yury Kaliada <fut.wrk@gmail.com>
2016-02-22 22:55:31 +0300

    fix redirect url
    Signed-off-by: Yury Kaliada <fut.wrk@gmail.com>

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
     uri = URI('https://api.worldoftanks.eu/wot/auth/login/')
     params = URI.encode_www_form({
       application_id: Rails.application.secrets.wargaming_app_id,
-      redirect_uri: 'http://127.0.0.1:3001/users/auth/wargaming/callback'
+      redirect_uri: "#{Rails.application.secrets.current_domain}/users/auth/wargaming/callback"
     })
     "#{uri}?#{params}"
   end
