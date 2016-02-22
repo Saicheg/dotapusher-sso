dd4d2773b2bbe6e82c4d7b6ac5cd33a45c2efedc
Yury Kaliada <fut.wrk@gmail.com>
2016-02-22 23:26:24 +0300

    fix sign up
    Signed-off-by: Yury Kaliada <fut.wrk@gmail.com>

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -9,9 +9,20 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # POST /resource
-  # def create
-  #   super
-  # end
+  def create
+    build_resource(sign_up_params)
+
+    resource.save
+    yield resource if block_given?
+    if resource.persisted?
+      expire_data_after_sign_in!
+      respond_with resource, location: after_inactive_sign_up_path_for(resource)
+    else
+      clean_up_passwords resource
+      set_minimum_password_length
+      respond_with resource
+    end
+  end
 
   # GET /resource/edit
   # def edit
